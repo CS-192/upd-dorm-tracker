5b87d9b9094c6c87654fd6bee02480dccd87c6e0
carlitomaso <carlatsopk44@gmail.com>
2025-02-19 20:37:15 +0800

    Make dashboard scrollable

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import styles from "./styles";
 import Ptext, { Ptitle } from "@/project_components";
@@ -15,35 +15,37 @@ const Dashboard = () => {
           { justifyContent: "flex-start", alignItems: "baseline" },
         ]}
       >
-        <Ptitle style={{ textAlign: "left", padding: 20, paddingBottom: 0 }}>
-          Dashboard
-        </Ptitle>
-        <DashboardGrid />
-        <DashboardCard>
-          <DashboardCard.Icon>
-            <AntDesign name="team" size={50} color="black" />
-          </DashboardCard.Icon>
-          <DashboardCard.Statistic>245/300</DashboardCard.Statistic>
-          <DashboardCard.Title>Number of People Inside</DashboardCard.Title>
-        </DashboardCard>
+        <ScrollView>
+          <Ptitle style={{ textAlign: "left", padding: 20, paddingBottom: 0 }}>
+            Dashboard
+          </Ptitle>
+          <DashboardGrid />
+          <DashboardCard>
+            <DashboardCard.Icon>
+              <AntDesign name="team" size={50} color="black" />
+            </DashboardCard.Icon>
+            <DashboardCard.Statistic>245/300</DashboardCard.Statistic>
+            <DashboardCard.Title>Number of People Inside</DashboardCard.Title>
+          </DashboardCard>
 
-        <DashboardCard>
-          <DashboardCard.Icon>
-            <AntDesign name="home" size={50} color="black" />
-          </DashboardCard.Icon>
-          <DashboardCard.Statistic>100/100</DashboardCard.Statistic>
-          <DashboardCard.Title>
-            Rooms are Currently Occupied
-          </DashboardCard.Title>
-        </DashboardCard>
+          <DashboardCard>
+            <DashboardCard.Icon>
+              <AntDesign name="home" size={50} color="black" />
+            </DashboardCard.Icon>
+            <DashboardCard.Statistic>100/100</DashboardCard.Statistic>
+            <DashboardCard.Title>
+              Rooms are Currently Occupied
+            </DashboardCard.Title>
+          </DashboardCard>
 
-        <DashboardCard>
-          <DashboardCard.Icon>
-            <AntDesign name="file1" size={50} color="black" />
-          </DashboardCard.Icon>
-          <DashboardCard.Statistic>23</DashboardCard.Statistic>
-          <DashboardCard.Title>New Request Submitted</DashboardCard.Title>
-        </DashboardCard>
+          <DashboardCard>
+            <DashboardCard.Icon>
+              <AntDesign name="file1" size={50} color="black" />
+            </DashboardCard.Icon>
+            <DashboardCard.Statistic>23</DashboardCard.Statistic>
+            <DashboardCard.Title>New Request Submitted</DashboardCard.Title>
+          </DashboardCard>
+        </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
