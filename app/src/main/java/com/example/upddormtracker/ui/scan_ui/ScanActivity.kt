9d237537684108ac4c8799e0c11a5042f58c7af9
kcthegreat18 <kfabriol1@up.edu.ph>
2025-04-09 05:30:02 +0800

    WIP: Integrate Scan UI to application

---
+++ app/src/main/java/com/example/upddormtracker/ui/scan_ui/ScanActivity.kt
@@ -0,0 +1,13 @@
+package com.example.upddormtracker.ui.scan_ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.upddormtracker.R
+
+class ScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_host)
+
+    }
+}
---
+++ app/src/main/java/com/example/upddormtracker/ui/scan_ui/SuccessfulScanFragment.kt
@@ -0,0 +1,19 @@
+package com.example.upddormtracker.ui.scan_ui
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.upddormtracker.R
+
+class SuccessfulScanFragment : Fragment(R.layout.fragment_successful_scan) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val scanAgainButton = view.findViewById<Button>(R.id.scan_again_button)
+        scanAgainButton.setOnClickListener {
+            findNavController().navigate(R.id.action_successfulScanFragment_to_scanFragment)
+        }
+    }
+}
