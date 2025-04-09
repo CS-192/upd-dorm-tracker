a9acf60f58f946a5df39890706c113864e7d37de
carlitomaso <carlatsopk44@gmail.com>
2025-04-10 03:59:38 +0800

    Create edit dormer feature
    For improvement: Improve consistency with add dormer layout - maybe implement the edit dormer details UI to the add dormer UI

--- app/src/main/java/com/example/upddormtracker/ui/managedormers/EditDormerFragment.kt
+++ app/src/main/java/com/example/upddormtracker/ui/managedormers/EditDormerFragment.kt
@@ -7,11 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import com.example.upddormtracker.R
 import com.example.upddormtracker.databinding.FragmentEditDormerBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
+@Suppress("DEPRECATION")
 class EditDormerFragment : Fragment() {
     private var _binding: FragmentEditDormerBinding? = null
     private val binding get() = _binding!!
@@ -34,6 +36,20 @@ class EditDormerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val dormer = arguments?.getSerializable("dormer") as? Dormer
+
+        setFragmentResultListener("requestKey") { requestKey, bundle ->
+            val updatedDormer = bundle.getSerializable("updatedDormer") as? Dormer
+
+            // If there's an updated dormer, update the UI with the new details
+            updatedDormer?.let {
+                binding.tvStudentNumber.text = "Student Number: ${it.studentNumber}"
+                binding.tvName.text = "Name: ${it.firstName} ${it.lastName}"
+                binding.tvDorm.text = "Dorm: ${it.dorm}"
+                binding.tvSex.text = "Sex: ${if (it.sex == "M") "Male" else "Female"}"
+                binding.tvBirthDate.text = "Birthday: ${it.birthday}"
+                binding.tvPhoneNumber.text = "Phone Number: ${it.phoneNumber}"
+            }
+        }
 
         dormer?.let {
             binding.tvStudentNumber.text = "Student Number: ${it.studentNumber}"
@@ -67,6 +83,16 @@ class EditDormerFragment : Fragment() {
                     }
                     .setNegativeButton("Cancel", null)
                     .show()
+            }
+        }
+
+        binding.btnUpdateDetails.setOnClickListener {
+            dormer?.let {
+                val bundle = Bundle()
+                bundle.putSerializable("dormer", it) // Pass the Dormer object
+
+                // Navigate to the editing fragment
+                findNavController().navigate(R.id.updateDormerDetailsFragment, bundle)
             }
         }
     }
