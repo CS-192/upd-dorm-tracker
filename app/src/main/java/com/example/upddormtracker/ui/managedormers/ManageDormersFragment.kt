89f1ccc61da6566752f2137d4a73c6b448f2f8f5
carlitomaso <carlatsopk44@gmail.com>
2025-05-05 15:04:10 +0800

    Fix dormers view
    Only query dormers who have the same dorm as the current user

--- app/src/main/java/com/example/upddormtracker/ui/managedormers/ManageDormersFragment.kt
+++ app/src/main/java/com/example/upddormtracker/ui/managedormers/ManageDormersFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.upddormtracker.R
+import com.example.upddormtracker.UserViewModel
 import com.example.upddormtracker.databinding.FragmentManageDormersBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -16,6 +18,9 @@ class ManageDormersFragment : Fragment() {
 
     private var _binding: FragmentManageDormersBinding? = null
     private val binding get() = _binding!!
+    private var userDorm: String? = null
+    private val userViewModel: UserViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,14 +30,19 @@ class ManageDormersFragment : Fragment() {
         _binding = FragmentManageDormersBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        // Add click listener for Potter's view action
+
 
         binding.addDormerButton.setOnClickListener {
             findNavController().navigate(R.id.addDormerFragment)
         }
 
         binding.rvDormers.layoutManager = LinearLayoutManager(requireContext())
-        loadDormers()
+        userViewModel.dorm.observe(viewLifecycleOwner) {
+            userDorm = it
+            loadDormers()
+        }
+
+
 
         return root
     }
@@ -42,6 +52,7 @@ class ManageDormersFragment : Fragment() {
         val dormerList = mutableListOf<Dormer>()
 
         db.collection("dormers")
+            .whereEqualTo("dorm", userDorm)
             .get()
             .addOnSuccessListener { documents ->
                 for (doc in documents) {
@@ -51,7 +62,8 @@ class ManageDormersFragment : Fragment() {
                 binding.rvDormers.adapter = DormerAdapter(dormerList)
             }
             .addOnFailureListener {
-                Toast.makeText(requireContext(), "Failed to load dormers", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Failed to load dormers", Toast.LENGTH_SHORT)
+                    .show()
             }
     }
 
